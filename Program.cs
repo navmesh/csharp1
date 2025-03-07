145619f72316c9a7f51635e5ae364db60f03a0cb
DESKTOP-M9U3OAM\Userxp <hoang747t@gmail.com>
2025-03-07 21:04:44 +0700

    sua lan 1

--- Program.cs
+++ Program.cs
@@ -5,4 +5,6 @@ tạo 1 repo trên github hoac gitlab
 dùng git client clone project về 1 empty folder 
 tải .gitignore https://gist.github.com/kmorcinek/2710267 và bỏ vào folder mà chúng ta vừa clone về (folder chứa .git) để hiện .git chúng ta phải show file và folder ẩn trên windows (trong folder option)
 tạo 1 project c sharp dùng IDE (visual studio) chú ý tạo trên 1 folder khác không liên quan đến folder chúng ta vừa clone về
+example
+
  */
